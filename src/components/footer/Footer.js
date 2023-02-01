8ce2c70aeb0588ae0fe74ff91fe01817fc55eace
Mechkour Hafida <79710349+Hafida-mh@users.noreply.github.com>
2023-02-01 16:00:34 +0100

    Update Footer.js

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -8,36 +8,26 @@ export default function Footer() {
     return (
         <div className="footer">
             <div className="footerleft">
-
-<div  className="tmsdemlogo"> 
-<img src={LogoTMSDEM} />
-</div>
-
-
+                <div  className="tmsdemlogo"> 
+                    <img src={LogoTMSDEM} />
+                 </div>
                 <div className="footerposition">
                     <div className="footerIcones"> <i class="fas fa-map-marker-alt"></i></div>
                     <div className="addressText"> 300 A Rue Marcel Paul, <section>94500 Champigny-sur-Marne, France </section></div>
                 </div>
-
                 <div className="footernumber">
                     <div className="footerIcones"> <img src={Phone} className="phone" /> </div>
                     <div className="addressText"> +33 1 41 77 11 32</div>
                 </div>
-
-
                 <div className="footeremail">
                     <div className="footerIcones"> <i class="fas fa-envelope"></i> </div>
                     <div className="addressText"> contact@tms-dem.com </div>
                 </div>
-
-
                 <div className="footerclientservice">
                     <div className="footerIcones"> <img src={SAV} className="sav" /> </div>
                     <div className="addressText"> Service client 7j/7</div>
                 </div>
-
             </div>
-       
         </div>
     )
 }
